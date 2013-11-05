43aeeae06d39e0b45a4f4bacd53f435950334a5c
Geir Kihle Hanssen <geir.kihle.hanssen@gmail.com>
2013-11-05 17:42:49 +0100

    Adapting code structure

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render                         # Import render, tool to send requests to specified URL along with accociated variables
-from django.conf import settings                            # Import settings file (settings.py) 
-from gallery.models import Pic                              # Import Pic from models
-from django.contrib.auth import authenticate, login, logout # Import django tools for user authentication
-from django.contrib.auth.decorators import login_required   # Import django tool for checking if user is authenticated or not
+# Import render, tool to send requests to specified URL along with 
+# accociated variables
+from django.shortcuts import render
+# Import settings file (settings.py)
+from django.conf import settings
+# Import Pic from models
+from gallery.models import Pic
+# Import django tools for user authentication
+from django.contrib.auth import authenticate, login, logout
+# Import django tool for checking if user is authenticated or not
+from django.contrib.auth.decorators import login_required
 
-# Function for what the front page should contain
 def index(request):
     # If user has pressed the logout button
     if request.GET.get('logout'):
@@ -12,7 +17,6 @@ def index(request):
         logout(request)
     # If user is authenticated
     if request.user.is_authenticated():
-        # Set latest to the 9 newest images from database
         latest = Pic.objects.filter(active=True).order_by('-id')[:9]
         return render(request,'frontpage/main.html',{'latest':latest})
     # Else if user is not authenticated, let user see the login screen
--- tags/views.py
+++ tags/views.py
@@ -5,24 +5,23 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def list_tags(request):
-    tags = Tag.objects.all().order_by('title') #QuerySet, fetches all tags from database sorted by their name
+    tags = Tag.objects.all().order_by('title')
     return render(request,'tags/listTags.html',{'tags':tags})
 
 @login_required
 def expand(request,t_id):
-    #Pick out only the images mentioned with tag_id in 't_id'.
-    image_list = Pic.objects.filter(picture_tag__tag = t_id, active = True).order_by('-id') #QuerySet, fetches images matching id from the tag-list.
+    image_list = Pic.objects.filter(picture_tag__tag = t_id, 
+                                    active = True).order_by('-id')
 
-    paginator = Paginator(image_list,48) #Initiate Paginator, a tool for page handling. Arguments (list of objects, desired number of objects per page)
-    page = request.GET.get('page') #Fetches current page from URL
+    paginator = Paginator(image_list,48)
+    page = request.GET.get('page')
 
     try:
-        images = paginator.page(page) #Returns objects belonging to desired page.
+        images = paginator.page(page)
     except PageNotAnInteger:
-        #If page is not an integer, deliver first page
         images = paginator.page(1)
     except EmptyPage:
-        #If page is out of range, deliver last page of results
         images = paginator.page(paginator.num_pages)
 
-    return render(request, 'tags/listPictures.html', {'images':images, 't_id':t_id})
+    return render(request, 'tags/listPictures.html', 
+        {'images':images, 't_id':t_id})
